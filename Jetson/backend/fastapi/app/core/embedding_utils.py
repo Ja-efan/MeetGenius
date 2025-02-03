83f686a9da9b9cf2c77f40ba8c2a9e83dbb909ed
연재환 <woghks1213@gmail.com>
2025-02-03 15:12:58 +0900

    chore(ETC): Web 디렉토리 삭제
    Jetson 브랜치 내 Web 디렉토리 삭제
    
    #S12P11B203-00

--- Jetson/backend/fastapi/app/core/embedding_utils.py
+++ Jetson/backend/fastapi/app/core/embedding_utils.py
@@ -6,11 +6,37 @@ embedding 생성 로직.
 import httpx
 import os
 from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModel
 
 # 환경 변수 로드
 load_dotenv()
 SOLAR_API_URL = os.getenv("SOLAR_API_URL")
 SOLAR_API_KEY = os.getenv("SOLAR_API_KEY")
+
+def get_embedding_model(model_path: str='nlpai-lab/KoE5', device: torch.device='cuda'):
+    """지정된 경로에서 임베딩 모델(예: KoE5)을 로드하여 토크나이저와 모델을 반환합니다.
+
+    Args:
+        model_path (str): 
+            사전 학습된 모델의 로컬 경로나 Hugging Face 모델 식별자.
+        device (torch.device): 
+            모델을 로드할 디바이스(CPU 또는 GPU).
+
+    Returns:
+        Tuple[AutoTokenizer, AutoModel]:
+            로드된 토크나이저와 모델을 포함하는 튜플. 주어진 디바이스에서 사용 가능.
+    """
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+    emb_model = AutoModel.from_pretrained(
+        model_path, 
+        # torch_dtype=torch.float16,  # fp16 사용 시 GPU 메모리 절감 가능
+        # low_cpu_mem_usage=True  # CPU 메모리 사용량 최소화
+        
+        
+    ).to(device)
+    emb_model.eval()
+    return tokenizer, emb_model
 
 async def get_embedding(text: str):
     """
