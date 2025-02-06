4a9bac8823fa28b630507092cc3fffc0a447c403
연재환 <woghks1213@gmail.com>
2025-02-07 01:09:55 +0900

    refactor(BE): projects.py 엔드포인트 내 클래스 삭제
    models/documents.py 로 모듈화
    
    #S12P11B203-217

--- Jetson/backend/fastapi/app/api/endpoints/projects.py
+++ Jetson/backend/fastapi/app/api/endpoints/projects.py
@@ -12,12 +12,15 @@
     body:
     {
         "document_id": str,
+        "document_name": str,   
         "project_id": str,
-        "document_name": str,
-        "document_content": str,
-        "document_type": str
+        "project_name": str,
+        "document_type": str,
+        "department_name": str | None,
+        "content": list[str],
+        "meeting_name": str | None,
+        "agenda_name": str | None
     }
-
     response:
     {
         "result": bool,
@@ -41,7 +44,7 @@
 """
 
 from fastapi import FastAPI, APIRouter, Depends
-from pydantic import BaseModel
+from models import Document, EmbeddingDocument, Metadata
 import json
 
 from core import chromadb_utils, llm_utils
@@ -51,37 +54,6 @@ router = APIRouter(
 )
 
 
-class DocumentBase(BaseModel):
-    """문서 기본 모델"""
-    document_id: str  # 문서 id
-    project_id: str  # 프로젝트 id
-    document_type: str  # 문서 타입
-    department_name: str | None  # 부서 이름
-    content: list[str]  # 문서 내용 (문장으로 구성된 리스트)
-    
-class Document(DocumentBase):
-    """벡터 DB에 저장할 문서 원본 모델"""
-    document_name: str  # 문서 이름
-    project_name: str  # 프로젝트 이름
-    meeting_name: str | None  # 회의 이름 (회의록 문서인 경우)
-    agenda_name: str | None  # 안건 이름 (회의록 문서인 경우)
-
-class Metadata(BaseModel):
-    """벡터 DB에 저장할 문서 메타데이터 모델"""
-    project_id: str | None # 프로젝트 id
-    project_name: str | None  # 프로젝트 이름
-    document_id: str  # 문서 id
-    document_name: str  # 문서 이름
-    document_type: str  # 문서 타입
-    meeting_name: str | None  # 회의 이름 (회의록 문서인 경우)
-    agenda_name: str | None  # 안건 이름 (회의록 문서인 경우)
-
-class EmbeddingDocument(BaseModel):
-    """벡터 DB에 저장할 문서 임베딩 모델"""
-    ids: str  # 각 벡터의 고유 ID -> 문서 id
-    documents: str  # 문서 내용  -> 텍스트 데이터 or list[str]
-    embeddings: list[list[float]]  # 임베딩 변환 된 문서 내용
-    metadatas: Metadata  # 문서 메타데이터
 
 @router.post("/{project_id}/documents")
 async def insert_project_document(document: Document, app: FastAPI = Depends()) -> json:
@@ -104,35 +76,30 @@ async def insert_project_document(document: Document, app: FastAPI = Depends()) 
     # 임베딩 모델 로드 
     model = llm_utils.load_embedding_model(app=app)
 
-    # 문서 내용 임베딩 변환 -> 현재 한 문장씩 임베딩 -> 배치 단위로 임베딩 할 수 있게 디벨롬
+    # 문서 내용 임베딩 변환 -> 현재 한 문장씩 임베딩 -> 배치 단위로 임베딩 할 수 있게 디벨롭되면 좋을 것 같다.
     embeddings = []
-    for sentence in document.document_content:
+    for sentence in document.content:
         embedding = model.encode([f"passage: {sentence}"])
         embeddings.append(embedding)
     
     embedding_document = EmbeddingDocument(
-        ids=[document_id],
+        ids=[document.document_id],
+        documents=document.content,
+        embeddings=embeddings,
         metadatas=[
             {
-                "document_id": document_id,  # 이걸 ids로 사용할 수 있을 것 같음
-                "project_id": project_id,  # 프로젝트 id
+                "document_id": document.document_id,  # 이걸 ids로 사용할 수 있을 것 같음
+                "project_id": document.project_id,  # 프로젝트 id
                 "project_name": document.project_name,  # 프로젝트 이름
                 "document_name": document.document_name,  # 문서 이름
                 "document_type": document.document_type,  # 문서 타입
 
             }
-        ],
-        documents=document.document_content,
-        embeddings=embeddings
+        ]
     )
     
     # 문서 정보 저장
-    project_collection.insert_data(
-        ids=[document_id],
-        metadatas=[{"source": document.document_name}],
-        documents=[document.document_content],
-        embeddings=embeddings
-    )
+    project_collection.insert_data([embedding_document])
 
     # 문서 삽입
     app.state.project_collection.insert_data(document)
