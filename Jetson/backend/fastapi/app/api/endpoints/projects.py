e2fe416ed60241541a099c4da7cdbb553ccb0bfe
연재환 <woghks1213@gmail.com>
2025-02-07 09:40:28 +0900

    feat(BE): 프로젝트 관련 문서 삭제 엔드포인트 정의
    프로젝트 관련 문서 ID 수신 받아 삭제
    
    #S12P11B203-213

--- Jetson/backend/fastapi/app/api/endpoints/projects.py
+++ Jetson/backend/fastapi/app/api/endpoints/projects.py
@@ -118,5 +118,10 @@ async def delete_project_document(project_id: str, document_id: str, app: FastAP
     Returns:
         json: 문서 삭제 여부 및 메시지
     """
+    # 프로젝트 컬렉션 
+    project_collection = chromadb_utils.get_project_collection(project_id=project_id)
+    
     # 문서 삭제
-    app.state.project_collection.delete_data(document_id)
+    project_collection.delete_data(document_id)
+    
+    return {"result": True, "message": f"문서({document_id}) 삭제 완료"}
