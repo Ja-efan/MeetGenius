c3f7f768ec68d7dccbce2c3d00a4b031da78a69e
연재환 <woghks1213@gmail.com>
2025-02-08 15:25:17 +0900

    feat(BE): AgendaBase, AgendaList, PrepareMeetingOut pedantic 모델 정의
    models/agendas -> models/meetings 모델 파일 병합 후 회의 준비 완료 모델 정의
    
    #S12P11B203-230

--- Jetson/backend/fastapi/app/models/meetings.py
+++ Jetson/backend/fastapi/app/models/meetings.py
@@ -1,30 +1,20 @@
-import datetime
-from datetime import datetime
-from typing import Optional, List
+""" 회의 및 안건 관련 모델 정의 """
+
 from pydantic import BaseModel, Field
 
 
 class AgendaBase(BaseModel):
-    """MariaDB Agenda Table"""
+    """안건 기본 모델"""
     agenda_id: int = Field(..., description="안건 id")
-    meeting_id: int = Field(..., description="회의 id")
-    title: str = Field(..., min_length=1, max_length=200, description="안건명")
-
-class Agenda(AgendaBase):
-    project_id: str = Field(..., description="프로젝트 id")
+    agenda_title: str = Field(..., description="안건명")
     
-class MeetingBase(BaseModel):
-    """MariaDB Meeting Table"""
-    meeting_id: id = Field(..., description="회의 id")
-    meeting_room: int | None = Field(None, description="회의실 번호")
-    meeting_title: str = Field(..., min_length=1, max_length=100, description="회의 제목")
-    meeting_date: datetime = Field(..., description="회의 날짜")
-    meeting_start_time: datetime = Field(..., description="회의 시작 시간")
-    meeting_end_time: datetime = Field(..., description="회의 종료 시간")
-    booker_id: str = Field(..., description="예약자 id")
-    project_id: str = Field(..., description="프로젝트 id")
     
-class Meeting(MeetingBase):
-    agendas: list[AgendaBase] | None = Field(default_factory=list, description="회의 안건 목록")
+class AgendaList(BaseModel):
+    project_id: str = Field(..., description="프로젝트 id")
+    agenda_list: list[AgendaBase] = Field(..., description="안건 목록")
 
 
+class PrepareMeetingOut(BaseModel):
+    """회의 준비 완료 모델"""
+    result: bool = Field(..., description="결과")
+    message: str = Field(..., description="메시지")
