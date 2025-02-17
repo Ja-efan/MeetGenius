d790977134ad349ae23768c809ded0d3b4115aaa
정찬호 <chano01794@gmail.com>
2025-02-17 13:49:24 +0900

    fix(BE): FastAPI 코드 정리 - main.py
    FastAPI 코드 정리
    
    #S12P11B203-00

--- Jetson/fastapi/app/main.py
+++ Jetson/fastapi/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.api.routes import meetings, projects, tests
+import ctypes
 
 app = FastAPI()
+
+ERROR_HANDLER_FUNC = ctypes.CFUNCTYPE(
+    None, ctypes.c_char_p, ctypes.c_int, ctypes.c_char_p, ctypes.c_int, ctypes.c_char_p
+)
+def py_error_handler(filename, line, function, err, fmt): # 에러 메시지를 무시함
+    pass
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+try:
+    asound = ctypes.cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+except Exception:
+    pass
 
 @app.get("/")
 def hello():
